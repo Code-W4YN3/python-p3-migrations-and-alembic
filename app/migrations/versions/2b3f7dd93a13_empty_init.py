5ad6fae331dd4e0635d4ed0f0e1ab30dd6e9930d
wayne-otido <otidowayne@gmail.com>
2023-09-01 18:04:51 +0300

    done

---
+++ app/migrations/versions/2b3f7dd93a13_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 2b3f7dd93a13
+Revises: 
+Create Date: 2023-09-01 17:39:52.490572
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2b3f7dd93a13'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
